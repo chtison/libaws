c56781ff5946421d4c4f0f105483eee48d101f4d
chtison <ch4rles.contact@gmail.com>
2017-02-05 15:47:49 +0100

    .

---
+++ CUSTOM-Cognito-User-Pool/README.md
@@ -0,0 +1,6 @@
+# CUSTOM-Cognito-User-Pool
+
+### Resource properties
+
+- **PoolName**: (**optional**) (not implemented)
+	* A name for the user pool. For valid values, see the <code>PoolName<code> parameter for the [CreateUserPool](https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_CreateUserPool.html) action in the *Amazon Cognito Your User Pools API Reference*. If you don't specify a name, a unique physical ID is generated and that ID is used for the pool name.
---
+++ CUSTOM-Cognito-User-Pool/lambda.py
@@ -0,0 +1,35 @@
+import boto3, httplib, json, requests
+
+cognitoIDP = boto3.client('cognito-idp')
+
+def handler(event, context):
+	if event['RequestType'] == 'Create':
+		create(event)
+	elif event['RequestType'] == 'Update':
+		update(event)
+	elif event['RequestType'] == 'Delete':
+		delete(event)
+
+def decorator(f):
+	def g(event):
+		data = {}
+		data['StackId'] = event['StackId']
+		data['RequestId'] = event['RequestId']
+		data['LogicalResourceId'] = event['LogicalResourceId']
+		data['PhysicalResourceId'] = event.get('PhysicalResourceId', data['RequestId']+data['LogicalResourceId'])
+		f(event, data)
+		data = json.dumps(data)
+		requests.put(event['ResponseURL'], data=data)
+	return g
+
+@decorator
+def create(event, data):
+	data['Status'] = 'SUCCESS'
+
+@decorator
+def update(event, data):
+	data['Status'] = 'SUCCESS'
+
+@decorator
+def delete(event, data):
+	data['Status'] = 'SUCCESS'
