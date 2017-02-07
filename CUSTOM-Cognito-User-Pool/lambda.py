a5e0b3ea4c28f8cdf71487d70b33c7d8cd0c4c48
chtison <ch4rles.contact@gmail.com>
2017-02-07 17:36:20 +0100

    .

--- CUSTOM-Cognito-User-Pool/lambda.py
+++ CUSTOM-Cognito-User-Pool/lambda.py
@@ -3,6 +3,8 @@ import boto3, httplib, json, requests
 cognitoIDP = boto3.client('cognito-idp')
 
 def handler(event, context):
+	print 'REQUEST:'
+	print json.dumps(event)
 	if event['RequestType'] == 'Create':
 		create(event)
 	elif event['RequestType'] == 'Update':
@@ -16,20 +18,43 @@ def decorator(f):
 		data['StackId'] = event['StackId']
 		data['RequestId'] = event['RequestId']
 		data['LogicalResourceId'] = event['LogicalResourceId']
-		data['PhysicalResourceId'] = event.get('PhysicalResourceId', data['StackId'].split('/')[1]+'-'+data['LogicalResourceId']+'-'+data['RequestId'])
+		if 'PhysicalResourceId' in event:
+			data['PhysicalResourceId'] = event['PhysicalResourceId']
+		data['Status'] = 'SUCCESS'
 		f(event, data)
-		data = json.dumps(data)
+		data = json.dumps(data, separators=(',', ':'))
+		print 'RESPONSE:'
+		print data
 		requests.put(event['ResponseURL'], data=data)
 	return g
 
 @decorator
 def create(event, data):
-	data['Status'] = 'SUCCESS'
+	if 'ResourceProperties' not in event:
+		event['ResourceProperties'] = {}
+	if 'ServiceToken' in event['ResourceProperties']:
+		del event['ResourceProperties']['ServiceToken']
+	if 'PoolName' not in event['ResourceProperties']:
+		event['ResourceProperties']['PoolName'] = data['StackId'].split('/')[1]+'-'+data['LogicalResourceId']
+	try:
+		response = cognitoIDP.create_user_pool(**event['ResourceProperties'])
+		data['PhysicalResourceId'] = response['UserPool']['Id']
+	except Exception as e:
+		data['Status'] = 'FAILED'
+		data['Reason'] = str(e)
+		return
+	return
 
 @decorator
 def update(event, data):
-	data['Status'] = 'SUCCESS'
+	return
 
 @decorator
 def delete(event, data):
-	data['Status'] = 'SUCCESS'
+	try:
+		cognitoIDP.delete_user_pool(
+			UserPoolId=data['PhysicalResourceId'],
+		)
+	except Exception as e:
+		print str(e)
+	return
